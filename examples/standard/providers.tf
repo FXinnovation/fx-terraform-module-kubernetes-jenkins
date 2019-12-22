d05a88b7eaedce51014b77e0dcce850db7739144
Quentin Vallin <quentin.vallin@fxinnovation.com>
2019-12-22 17:06:55 -0500

    fix/ terraform 0.12 syntax

--- examples/standard/providers.tf
+++ examples/standard/providers.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   version    = "~> 2.27.0"
   region     = "us-east-2"
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 provider "random" {
